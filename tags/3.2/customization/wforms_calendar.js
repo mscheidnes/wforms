40af890712e315b7a82c15b3f3266ad94f358b32
cedric.savarese <cedric.savarese@709aefc4-6f42-0410-a42d-23c4347884e8>
2010-06-14 18:27:43 +0000

    tagging 3.2 release

---
+++ tags/3.2/customization/wforms_calendar.js
@@ -0,0 +1,157 @@
+new function(_) {
+	
+	if(typeof YAHOO == 'undefined') { return false; }
+	
+	if(!wFORMS.helpers.calendar) {wFORMS.helpers.calendar = {}};
+	wFORMS.helpers.calendar.alreadyApplied = false;
+    
+	//Store old onApply behavior for reuse.	
+	var _onApply = wFORMS.behaviors.validation.instance.prototype.onApply;
+    
+	if(wFORMS.behaviors.validation) {
+    
+		wFORMS.behaviors.validation.rules.isDate.selector  = ".validate-date, .validate-datecal";
+				
+		wFORMS.behaviors.validation.instance.prototype.onApply = function(){
+	
+	        if(wFORMS.helpers.calendar.alreadyApplied == false){        //Ensure init is run only once
+	            wFORMS.helpers.calendar.calendar_init();        
+	            wFORMS.helpers.calendar.alreadyApplied = true;
+	        }
+	        
+			if(_onApply) _onApply.apply(this);
+		}
+	}
+    
+	if(wFORMS.behaviors.repeat) {
+		var _onRepeat = wFORMS.behaviors.repeat.onRepeat;	
+    
+		wFORMS.behaviors.repeat.onRepeat = function(e){
+			if(_onRepeat) _onRepeat.apply(e);
+        
+            	var cal = wFORMS.helpers.calendar.instance[wFORMS.helpers.calendar.instance.length-1];
+                
+			var datesList = base2.DOM.Element.querySelectorAll(e,'.validate-datecal');
+			datesList.forEach(function(f){
+				YAHOO.util.Event.addListener(f.id, "focus", YAHOO.formmanager.calendar.showCal, cal, true);
+				YAHOO.util.Event.addListener(f.id, "blur", YAHOO.formmanager.calendar.hideCal, cal, true);
+			});      
+		}
+	}    
+		
+	wFORMS.helpers.calendar.calendar_init = function(){
+		
+		YAHOO.namespace("formmanager.calendar");
+		
+		YAHOO.formmanager.calendar.handler = function(type,args,cal) {
+			if(type=='select') {	
+			
+				var dates = this.getSelectedDates();
+				if(dates){
+					//var selectedDate = (dates[0].getMonth()+1) + "/" + dates[0].getDate() + "/" + dates[0].getFullYear();
+					var selectedDateArray = Array();
+					selectedDateArray[this.Locale.MDY_DAY_POSITION-1] = dates[0].getDate();
+					selectedDateArray[this.Locale.MDY_MONTH_POSITION-1] = dates[0].getMonth()+1;
+					selectedDateArray[this.Locale.MDY_YEAR_POSITION-1] = dates[0].getFullYear();
+					var seperator = 	this.Locale.DATE_FIELD_DELIMITER;
+					
+					
+					var selectedDate = selectedDateArray.join(seperator);
+					cal._targetField.value = selectedDate;
+					this.hide();
+				}
+			}
+			else if(type=='deselect') {			
+					cal._targetField.value = "";
+			}
+		};
+		
+		YAHOO.formmanager.calendar.today   = new Date();
+		YAHOO.formmanager.calendar.mindate = (YAHOO.formmanager.calendar.today.getMonth()+1) + "/" + YAHOO.formmanager.calendar.today.getDate() + "/" + YAHOO.formmanager.calendar.today.getFullYear();
+		
+		YAHOO.formmanager.calendar.showCal = function (e, cal) {
+			this.hide();
+			if(!e.target){e.target = e.srcElement;};	//Clean IE's weird nomenclature
+		
+		
+			var field  = e.target;
+			cal._targetField = field;
+			var caldiv = cal.oDomContainer;
+	
+			caldiv.style.display='block';
+			var fp = wFORMS.helpers.position(field);
+			var cp = wFORMS.helpers.position(caldiv);
+			
+			var diff = {
+				left: fp.left - cp.left + 10,
+				top: fp.top - cp.top
+			};
+			caldiv.style.left = caldiv.offsetLeft + diff.left + field.offsetWidth +'px';
+			caldiv.style.top = caldiv.offsetTop + diff.top +'px';
+									
+			this.show();
+		}
+	
+		YAHOO.formmanager.calendar.hideCal = function (e,f) {
+			if(f.over_cal == false){
+				f._targetField = null;
+				f.hide();
+			}
+		}
+		
+		YAHOO.formmanager.calendar.setLocale = function(cal) {
+			if(wFORMS.helpers.calendar && wFORMS.helpers.calendar.locale){
+				for(property in wFORMS.helpers.calendar.locale){
+					cal.cfg.setProperty(property, wFORMS.helpers.calendar.locale[property]); 
+				}
+			}
+		}
+	
+		
+		wFORMS.helpers.calendar.cal_init = function(){
+			var forms=base2.DOM.Element.querySelectorAll(document,'.wForm form');
+        
+			for(i =0; i < forms.length; i++){
+            
+				if(!forms.item(i).querySelectorAll) {
+					base = base2.DOM.bind(forms[i]);
+				}
+				var newdiv = document.createElement("div")
+				newdiv.id = forms.item(i).id+"_calContainer";
+				forms.item(i).appendChild(newdiv);
+					
+				if(wFORMS.helpers.calendar && wFORMS.helpers.calendar.title)
+				{
+					var title_name = wFORMS.helpers.calendar.title;
+				}else{
+					var title_name = "Please select a date";
+				}
+	
+			if(!(wFORMS.helpers.calendar.instance)){
+                wFORMS.helpers.calendar.instance = new Array();
+			}
+
+            var cal = new YAHOO.widget.Calendar("cal",newdiv.id, { title: title_name, close:true});
+            wFORMS.helpers.calendar.instance.push(cal);
+            cal = wFORMS.helpers.calendar.instance[wFORMS.helpers.calendar.instance.length-1];
+				YAHOO.formmanager.calendar.setLocale(cal);
+	
+				cal.selectEvent.subscribe(YAHOO.formmanager.calendar.handler, cal, true);
+				cal.deselectEvent.subscribe(YAHOO.formmanager.calendar.handler, cal, true);
+                cal.render();
+				cal.hide();
+				cal.over_cal = false;
+				
+				YAHOO.util.Event.addListener(cal.containerId, "mouseover", function(){this.over_cal = true;},cal,true);
+				YAHOO.util.Event.addListener(cal.containerId, "mouseout", function(){this.over_cal = false;},cal,true);
+				
+				var datesList = base2.DOM.Element.querySelectorAll(forms.item(i),'.validate-datecal');
+				datesList.forEach(function(f){
+					YAHOO.util.Event.addListener(f.id, "focus", YAHOO.formmanager.calendar.showCal, cal, true);
+					YAHOO.util.Event.addListener(f.id, "blur", YAHOO.formmanager.calendar.hideCal, cal, true);
+				});
+			}
+		}      
+        wFORMS.helpers.calendar.cal_init();
+	}
+}();
---
+++ tags/3.2/wforms_hint.js
@@ -0,0 +1,161 @@
+
+if (typeof(wFORMS) == "undefined") {
+	throw new Error("wFORMS core not found. This behavior depends on the wFORMS core.");
+}
+/**
+ * wForms hint behavior. Show/highlight an HTML element when the associated input gets the focus.
+ */
+wFORMS.behaviors.hint  = { 
+	
+	/**
+	 * Inactive CSS class for the element
+     * @final
+	 */
+	CSS_INACTIVE : 'field-hint-inactive',
+
+	/**
+	 * Active CSS class for the element
+     * @final
+	 */
+	CSS_ACTIVE : 'field-hint',
+
+	/**
+	 * Selector expression for the hint elements
+     * @final
+     * @see	http://www.w3.org/TR/css3-selectors/
+	 */
+	HINT_SELECTOR : '*[id$="-H"]',
+
+	/**
+	 * Suffix of the ID for the hint element
+     * @final
+	 */
+	HINT_SUFFIX : '-H',
+
+	/**
+	 * Creates new instance of the behavior
+     * @constructor
+	 */
+	instance : function(f) {
+		this.behavior = wFORMS.behaviors.hint; 
+		this.target = f;
+	}
+}
+
+/**
+ * Factory Method.
+ * Applies the behavior to the given HTML element by setting the appropriate event handlers.
+ * @param {domElement} f An HTML element, either nested inside a FORM element or (preferably) the FORM element itself.
+ * @return {object} an instance of the behavior 
+ */	
+wFORMS.behaviors.hint.applyTo = function(f) {
+	var b = new wFORMS.behaviors.hint.instance(f);
+	// Selects all hints elements using predefined selector and attaches
+	// event listeners to related HTML elements for each hint
+	f.querySelectorAll(wFORMS.behaviors.hint.HINT_SELECTOR).forEach(
+		function(elem){
+			
+			// ID attribute is not checked here because selector already contains it
+			// if selector is changed, ID check should also exists
+			// if(!elem.id) { return ; }
+			var e = b.getElementByHintId(elem.id);
+			if(e){
+				if(!e.addEventListener) base2.DOM.bind(e);
+				if(e.tagName == "SELECT" || e.tagName == "TEXTAREA" || (e.tagName == "INPUT" && e.type != "radio" && e.type != "checkbox")){							
+					e.addEventListener('focus', function(event) { b.run(event, this)}, false);
+					e.addEventListener('blur',  function(event) { b.run(event, this)}, false);	
+				} else {
+					e.addEventListener('mouseover', function(event) { b.run(event, e)}, false);
+					e.addEventListener('mouseout', function(event) { b.run(event, e)}, false);
+				}
+			}
+		}
+	);
+	b.onApply();
+	return b;
+}
+
+/**
+ * Executed once the behavior has been applied to the document.
+ * Can be overwritten.
+ */
+wFORMS.behaviors.hint.instance.prototype.onApply = function() {} 
+
+/**
+ * Executes the behavior
+ * @param {event} event
+ * @param {domElement} elem
+ */
+wFORMS.behaviors.hint.instance.prototype.run = function(event, element) { 	
+	
+	var hint = this.getHintElement(element);
+	if(!hint) return;
+	if(!hint.removeClass) base2.DOM.bind(hint);
+	
+	if(event.type == 'focus' || event.type == 'mouseover'){
+		hint.removeClass(wFORMS.behaviors.hint.CSS_INACTIVE)
+		hint.addClass(wFORMS.behaviors.hint.CSS_ACTIVE);
+		if (!wFORMS.helpers.getForm(element).hasClass('hintsSide')) {
+			this.setup(hint, element);
+		}
+	} else{
+		hint.addClass(wFORMS.behaviors.hint.CSS_INACTIVE);
+		hint.removeClass(wFORMS.behaviors.hint.CSS_ACTIVE);
+	}
+}
+
+
+/**
+ * Returns HTMLElement related to specified hint ID
+ * @returns	{HTMLElement}
+ */
+wFORMS.behaviors.hint.instance.prototype.getElementByHintId = function(hintId){
+	var id = hintId.substr(0, hintId.length - wFORMS.behaviors.hint.HINT_SUFFIX.length);
+	var e = document.getElementById(id);
+	return e;
+}
+
+/**
+ * Returns HTMLElement Hint element associated with element event catched from
+ * @returns	{HTMLElement}
+ */
+wFORMS.behaviors.hint.instance.prototype.getHintElement = function(element){
+	var e = document.getElementById(element.id + this.behavior.HINT_SUFFIX);
+	if(e && !e.hasClass){base2.DOM.bind(e);}
+	return e && e != '' ? e : null;
+}
+
+/**
+ * Setups hint position on the screen depend on the element
+ * @param	{HTMLElement}	hint	Hint HTML element
+ * @param   {HTMLElement}	source	HTML element with focus.
+ */
+wFORMS.behaviors.hint.instance.prototype.setup = function(hint, field) {
+	var form = wFORMS.helpers.getForm(field);
+	if (hint.parentNode != form) {
+		form.appendChild(hint);
+	}
+	var fp = wFORMS.helpers.position(field);
+	var hp = wFORMS.helpers.position(hint);
+	var diff = {
+		left: fp.left - hp.left,
+		top: fp.top - hp.top
+	};
+	if (field.tagName.toLowerCase() == 'select') {
+		hint.style.left = hint.offsetLeft + diff.left + field.offsetWidth +'px';
+		hint.style.top = hint.offsetTop + diff.top +'px';
+	} else {
+		hint.style.left = hint.offsetLeft + diff.left +'px';
+		hint.style.top = hint.offsetTop + diff.top + field.offsetHeight +'px';
+	}
+}
+
+/**
+ * Returns if ID is of the HINT element. Used by repeat behavior to correctly 
+ * update hint ID
+ * @param	{DOMString}	id
+ * @return	boolean
+ */
+wFORMS.behaviors.hint.isHintId = function(id){
+	return id.match(new RegExp(wFORMS.behaviors.hint.HINT_SUFFIX + '$')) != null;
+}
