e7eb4056d3dcfe8cb0205dc3e010f9ab14184dc0
anchangsi@gmail.com <anchangsi@gmail.com@709aefc4-6f42-0410-a42d-23c4347884e8>
2012-11-13 19:33:48 +0000

    introducing Jasmine unit test framework

---
+++ branches/r426-length-prompt-fix/test/jasmine/jasmine-starter.js
@@ -0,0 +1,36 @@
+/**
+ * User: anch
+ * Date: 11/13/12
+ * Time: 12:40 PM
+ */
+/**
+ * jasmine boilerplate
+ */
+(function () {
+    'use strict';
+
+    var jasmineEnv = jasmine.getEnv();
+    jasmineEnv.updateInterval = 250;
+
+    var htmlReporter = new jasmine.HtmlReporter();
+    jasmineEnv.addReporter(htmlReporter);
+
+    jasmineEnv.specFilter = function (spec) {
+        return htmlReporter.specFilter(spec);
+    };
+
+    jasmine.getFixtures().fixturesPath = 'fixtures';
+
+    $(jasmine).on('fixture-loaded', function(event, fixtureFileName){
+        wFORMS.initialized = false;
+        wFORMS.onLoadHandler();
+    });
+
+    $(document).ready(function(){
+        execJasmine();
+    });
+
+    function execJasmine() {
+        jasmineEnv.execute();
+    }
+})();
