70fdc3aa0c5e75f88eeb536d8020b37851c2be0a
anchangsi <anchangsi@gmail.com>
2012-11-19 22:31:19 +0000

    fix #912

--- test/jasmine/jasmine-starter.js
+++ test/jasmine/jasmine-starter.js
@@ -20,7 +20,7 @@
     };
 
     jasmine.getFixtures().fixturesPath = 'fixtures';
-    jasmine.getFixtures().cleanFixtureAfterEach = false;
+//    jasmine.getFixtures().cleanFixtureAfterEach = false;
 
     $(jasmine).on('fixture-loaded', function(event, fixtureFileName){
         wFORMS.initialized = false;
@@ -35,3 +35,33 @@
         jasmineEnv.execute();
     }
 })();
+
+var UTIL = function(UTIL){
+
+    return $.extend(UTIL, {
+        dispatchKeypressEvent: function(element, keyCode, charCode){
+            // Create the event
+            var evt = document.createEvent( 'KeyboardEvent' );
+
+            // Init the options
+            evt.initKeyEvent(
+                "keypress",        //  the kind of event
+                true,             //  boolean "can it bubble?"
+                true,             //  boolean "can it be cancelled?"
+                null,             //  specifies the view context (usually window or null)
+                false,            //  boolean "Ctrl key?"
+                false,            //  boolean "Alt key?"
+                false,            //  Boolean "Shift key?"
+                false,            //  Boolean "Meta key?"
+                keyCode || 0,               //  the keyCode
+                charCode || 0);              //  the charCode
+
+            // Dispatch the event on the element
+            element.dispatchEvent( evt );
+        },
+
+        getValue: function(s){
+            return parseFloat(('' + s).replace(/px$/i, ''));
+        }
+    });
+}(UTIL || {});
