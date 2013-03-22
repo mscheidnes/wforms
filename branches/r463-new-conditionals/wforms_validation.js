924e405b96f1cec9ed5b212b8eb3fd887c815d5f
cedric.savarese@gmail.com <cedric.savarese@gmail.com@709aefc4-6f42-0410-a42d-23c4347884e8>
2013-03-22 22:00:59 +0000

    fix unit tests for validation behavior.

--- branches/r463-new-conditionals/wforms_validation.js
+++ branches/r463-new-conditionals/wforms_validation.js
@@ -670,13 +670,11 @@ wFORMS.behaviors.validation.instance.prototype.validateDateTime = function(eleme
 };
 
 wFORMS.behaviors.validation.instance.prototype.validateDate = function(element, value) {
-    console.log('validate-date:' + value);
     if (this.isEmpty(value)) {
         return true;
     }
     var date = this.analyzeDateComponents(value);
-    console.log('date:');
-    console.log(date);
+
     return !(date === null);
 };
 /**
@@ -694,8 +692,6 @@ wFORMS.behaviors.validation.instance.prototype.analyzeDateComponents = function(
             MDY_YEAR_POSITION : 3
         }
     }
-
-    console.log(cfg);
 
     var splitter = /[\/\.\-\s]/;
     var dArr = value.split(splitter);
